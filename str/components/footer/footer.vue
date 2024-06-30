ebb94be372d75c5cc40df1ece8326df7710bc217
jiemou5 <1330205357@qq.com>
2024-06-30 16:36:13 +0800

    cbr003

---
+++ str/components/footer/footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="footer">
+    <div class="big_title">
+        SICH UP SPECIAL PROMOTIONS
+    </div>
+    <div class="small_title">
+        Get exclusive deals you want find anywhere else stright to your inbox!
+    </div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="less"  type="text/less">
+@import '../../common/less/index.less';
+#footer{
+    background: @main_color;
+    color: #FFF;
+    text-align: center;
+    .big_title{
+        margin-top: 15px;
+        line-height: 25px;
+        font-size: @p3;
+        font-weight: bold;
+        letter-spacing: 1px;
+    }
+    .small_title{
+        margin-bottom: 10px;
+        line-height: 15px;
+        font-size: @min_size;
+        letter-spacing: 1px;
+    }
+}
+</style>
---
+++ str/components/main/main.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="main">
+    <!-- <carouselfigure v-show="true" :img-data=img_data></carouselfigure> -->
+    <vheader></vheader>
+    <div class="main_content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+<script>
+import carousel_figure from "../carousel_figure/carousel_figure"
+import header from "../header/heade"
+export default {
+  data() {
+    return {
+      img_data: [".Image/banner2.png",".Image/banner3.png"]
+    }
+  },
+  components: {
+    carouselfigure: carousel_figure,
+    vheader: header
+  }
+}
+</script>
+<style lang="less"  type="text/less">
+#main{
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  .main_content{
+    flex: 1;
+  }
+}
+</style>
